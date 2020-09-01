7b7b14d317b24667cc84f9cb5ec2b8906bbf42a8
Chabane Youcef El Amine <joechabane98@gmail.com>
2020-09-01 03:05:08 +0200

    connected the app to fcm

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        print('onMessage called: $message');
+      },
+      onResume: (Map<String, dynamic> message) {
+        print('onResume called: $message');
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        print('onLaunch called: $message');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
