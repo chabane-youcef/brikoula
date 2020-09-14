7b01889d0dd93a03a1cc85e636f5414f6f780cb2
Chabane Youcef El Amine <joechabane98@gmail.com>
2020-09-14 22:31:31 +0200

    added profile screen

---
+++ lib/ui/profile_screen.dart
@@ -0,0 +1,156 @@
+import 'package:brikoula_client_app/components/demand_card.dart';
+import 'package:brikoula_client_app/constants/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [Image.asset('assets/icons/ic_options.png')],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              CircleAvatar(
+                radius: 50,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Chabane youcef',
+                maxLines: 1,
+                style: TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Card(
+                elevation: 3,
+                semanticContainer: true,
+                margin: EdgeInsets.all(0),
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8, vertical: 2),
+                  child: Text(
+                    'membre since mars 2016',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 90,
+                child: Card(
+                  elevation: 3,
+                  semanticContainer: true,
+                  margin: EdgeInsets.all(0),
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Image.asset('assets/icons/ic_phone.png'),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              '0697422421',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w700),
+                            ),
+                          ],
+                        ),
+                        VerticalDivider(
+                          color: AppColors.primary,
+                        ),
+                        Column(
+                          children: [
+                            Image.asset('assets/icons/ic_home_big.png'),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Ain yagout',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w700),
+                            ),
+                          ],
+                        ),
+                        VerticalDivider(
+                          color: AppColors.primary,
+                        ),
+                        Column(
+                          children: [
+                            Image.asset('assets/icons/ic_demandes.png'),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              '14 demandes',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w700),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Previews demands:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  ),
+                  Container(
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        DemandCard(name: 'boufar tarek',category: 'plumber',date: '12-12-2019',period: 'morning',status: 'approved',),
+                        DemandCard(name: 'boufar tarek',category: 'plumber',date: '12-12-2019',period: 'morning',status: 'refused',),
+                        DemandCard(name: 'boufar tarek',category: 'plumber',date: '12-12-2019',period: 'morning',status: 'waiting',),
+                        DemandCard(name: 'boufar tarek',category: 'plumber',date: '12-12-2019',period: 'morning',status: 'refused',),
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
