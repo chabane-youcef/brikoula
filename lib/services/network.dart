0f071da86ffa8f55844448e0eb4183a7da27cb70
Chabane Youcef El Amine <joechabane98@gmail.com>
2020-09-15 22:53:31 +0200

    added register api call

--- lib/services/network.dart
+++ lib/services/network.dart
@@ -14,15 +14,52 @@ class Network {
 
     String data = response.body;
     var responseData = json.decode(data);
-    if(response.statusCode == 200){
-      return AuthResponse(responseData['status'], responseData['token'],);
-    }else if(response.statusCode == 401){
-      return AuthResponse.failed(responseData['status'], responseData['message']);
-    }else {
+    if (response.statusCode == 200) {
+      return AuthResponse(
+        responseData['status'],
+        responseData['token'],
+      );
+    } else if (response.statusCode == 401) {
+      return AuthResponse.failed(
+          responseData['status'], responseData['message']);
+    } else {
       print(response.statusCode);
       print(response.body);
       return AuthResponse.failed(
           responseData['status'], responseData['message']);
+    }
+  }
+
+  Future<AuthResponse> register(
+      {@required String route,
+      @required String name,
+      @required String phone,
+      @required String email,
+      @required String password}) async {
+    server.Response response = await server.post(
+        'baseurk$route?name=$name&phone=$phone&email=$email&password=$password');
+
+    String data = response.body;
+    print(data);
+    var responseData = jsonDecode(data);
+
+    if (response.statusCode == 200) {
+      return AuthResponse(
+        responseData['status'],
+        responseData['token'],
+      );
+    } else if (response.statusCode == 401) {
+      return AuthResponse.failed(
+        responseData['status'],
+        responseData['message'],
+      );
+    }else {
+      print(responseData['status']);
+      print(responseData['message']);
+      return AuthResponse.failed(
+        responseData['status'],
+        responseData['message'],
+      );
     }
   }
 }
